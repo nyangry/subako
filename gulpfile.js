38f3750cc901497540b7c9a6f2d3b2fd2ee9865a
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2015-10-26 12:41:22 +0900

    Implement gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,6 @@ var browserify = require('gulp-browserify');
 var jade       = require('gulp-jade');
 var uglify     = require('gulp-uglify');
 var rename     = require('gulp-rename');
+
+var dist = './dist';
+var src  = './src';
