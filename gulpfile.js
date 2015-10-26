81ab9af55d63371e1d55c17de71f6fa67fbca7ac
Daisuke Masuyama <iacta.alea.esto@gmail.com>
2015-10-26 12:41:11 +0900

    Add node modules

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,4 @@ var gulp       = require('gulp');
 var browserify = require('gulp-browserify');
 var jade       = require('gulp-jade');
 var uglify     = require('gulp-uglify');
+var rename     = require('gulp-rename');
